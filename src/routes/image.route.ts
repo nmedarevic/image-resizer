e46086649bc63b90cf5a29991add400f72129b76
Nikola Medarevic <nikola@petmediagroup.com>
2025-03-30 11:33:13 +0200

    Params guard

--- src/routes/image.route.ts
+++ src/routes/image.route.ts
@@ -8,6 +8,24 @@ router.get('/resize/:width/:height/:urlEncodedUrl', async (req, res, next) => {
   const { width, height, urlEncodedUrl } = req.params;
   console.log('\n\n', req.params, '\n\n');
 
+  const parsedWidth = Number(width) 
+  const parsedHeight = Number(height) 
+
+  console.log('\n\n', parsedWidth, '\n\n');
+  console.log('\n\n', parsedHeight, '\n\n');
+  
+  if (parsedWidth === 0 || isNaN(parsedWidth)) {
+    res.status(400)
+    res.send("Invalid width")
+    return;
+  }
+
+  if (parsedHeight === 0 || isNaN(parsedHeight)) {
+    res.status(400)
+    res.send("Invalid height")
+    return;
+  }
+
   const url = decodeURIComponent(urlEncodedUrl)
 
   const imageResponse = await fetch(url)
