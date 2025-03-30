b8e2a462c28d1e63b0744033620b65ecc7e1a2ad
Nikola Medarevic <nikola@petmediagroup.com>
2025-03-30 12:37:56 +0200

    Small change

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,6 @@
 import express from 'express'
 import imageRoutes from './routes/image.route'
-// import basicRoutes from './routes/basic.route'
 
 export const addRoutes = (app: express.Express) => {
   app.use(imageRoutes)
-  // app.use(basicRoutes)
 }
--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ const server = app.listen(port, () => {
 
 process.on('SIGTERM', () => {
   debug('SIGTERM signal received: closing HTTP server')
+
   server.close(() => {
     debug('HTTP server closed')
   })
